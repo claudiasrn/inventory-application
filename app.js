13b57b1b7ed044727727f377fe73dcc98106476a
claudiasrn <perezserranoclaudia@gmail.com>
2026-09-06 18:57:30 +0200

    feat: add index, category and item routers

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const path = require("node:path");
 const express = require("express");
+const indexRouter = require("./routes/indexRouter");
+const categoryRouter = require("./routes/categoryRouter");
+const itemRouter = require("./routes/itemRouter");
 
 const app = express();
 
@@ -9,6 +12,10 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")))
+
+app.use("/", indexRouter);
+app.use("/categories", categoryRouter);
+app.use("/items", itemRouter);
 
 app.listen(process.env.PORT || 8080, () => {
 	console.log("Server running");
---
+++ routes/itemRouter.js
@@ -0,0 +1,4 @@
+const { Router } = require("express");
+const itemRouter = Router();
+
+module.exports = itemRouter;
