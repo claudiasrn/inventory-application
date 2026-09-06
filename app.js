1efd9dc87b85f60d1ce5ba0d457123f7f865cf01
claudiasrn <perezserranoclaudia@gmail.com>
2026-09-06 18:03:06 +0200

    feat: set up express app with ejs and static assets

---
+++ app.js
@@ -0,0 +1,15 @@
+const path = require("node:path");
+const express = require("express");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.listen(process.env.PORT || 8080, () => {
+	console.log("Server running");
+});
