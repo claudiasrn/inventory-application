cd3d03ab2ecd418b9042ea74aa04adaa7397f770
claudiasrn <perezserranoclaudia@gmail.com>
2026-09-13 22:06:42 +0200

    feat: add item detail controller

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -5,6 +5,18 @@ async function getItems(req, res) {
 	res.render("items", { items });
 }
 
+async function getItem(req, res) {
+	const id = Number(req.params.id);
+
+	const [item, suppliers] = await Promise.all([
+		db.getItemById(id),
+		db.getSuppliersByItem(id),
+	]);
+
+	res.render("item", { item, suppliers });
+}
+
 module.exports = {
 	getItems,
+	getItem,
 };
