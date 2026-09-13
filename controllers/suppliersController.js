84d8b8380bea60685354ed6bff2c1bcb231102b5
claudiasrn <perezserranoclaudia@gmail.com>
2026-09-13 22:32:59 +0200

    feat: add supplier detail controller

--- controllers/suppliersController.js
+++ controllers/suppliersController.js
@@ -5,6 +5,18 @@ async function getSuppliers(req, res) {
 	res.render("suppliers", { suppliers });
 }
 
+async function getSupplier(req, res) {
+	const id = Number(req.params.id);
+
+	const [supplier, items] = await Promise.all([
+		db.getSupplierById(id),
+		db.getItemsBySupplier(id),
+	]);
+
+	res.render("supplier", { supplier, items });
+}
+
 module.exports = {
 	getSuppliers,
+	getSupplier,
 };
