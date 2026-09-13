bfbf594bdd0e4375c75662159ab7631141e328cf
claudiasrn <perezserranoclaudia@gmail.com>
2026-09-13 22:17:05 +0200

    feat: mount suppliers router

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const indexRouter = require("./routes/indexRouter");
 const categoryRouter = require("./routes/categoriesRouter");
 const itemRouter = require("./routes/itemsRouter");
+const supplierRouter = require("./routes/suppliersRouter");
 
 const app = express();
 
@@ -11,11 +12,12 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/categories", categoryRouter);
 app.use("/items", itemRouter);
+app.use("/suppliers", supplierRouter);
 
 app.listen(process.env.PORT || 8080, () => {
 	console.log("Server running");
